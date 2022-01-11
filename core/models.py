5b783a62b01c56ac0659a57a48490a412e88123a
bmargerison <bmargerison@outlook.com>
2022-01-11 17:42:31 +0000

    Pet class and one-to-many relationship with Shelter class

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.base import Model
 
+
 # Create your models here.
 class Shelter(models.Model):
     name = models.CharField(max_length=100)
@@ -8,5 +9,12 @@ class Shelter(models.Model):
     password = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
+class Pet(models.Model):
+    shelter = models.ForeignKey(Shelter, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    
     def __str__(self):
         return self.name
--- core/serializers.py
+++ core/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
-from .models import Shelter
+from .models import Shelter, Pet
 
 class ShelterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shelter
         fields = '__all__'
 
-
+class PetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pet
+        fields = '__all__'
