5e597db2d8b094f0c7808863494f5d1df6405fff
bmargerison <bmargerison@outlook.com>
2022-01-13 10:53:07 +0000

    React hook to display pet names

--- frontend/src/components/ShowPets.js
+++ frontend/src/components/ShowPets.js
@@ -1,14 +1,35 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import '../App.css';
-import axios from "axios";
 
-class ShowPets extends Component {
-  render() {
-    return (
-      <p>Hello from Show Pets</p>
-    )
-};
+const ShowPets = () => {
+  const [pets, setData] = useState([]);
 
+  useEffect(() => {
+    const url = "http://localhost:8000/api/pet/";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className='all_pets'>
+      {pets.map((pet, index) => (
+        <div className='pet' key={pet.id}>
+          <p className='name'>{pet.name}</p>
+          <br></br>
+        </div>
+      ))}
+    </div>
+  )
 };
 
 export default ShowPets;
