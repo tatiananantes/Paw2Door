5169ca01f1050080d9244a32ccef6db9e081746b
bmargerison <bmargerison@outlook.com>
2022-01-17 16:03:31 +0000

    Shelter details appear on pet profile page

--- frontend/src/components/PetDetails.js
+++ frontend/src/components/PetDetails.js
@@ -1,16 +1,16 @@
+import axios from "axios";
 import React, {useEffect, useState } from "react";
 import '../App.css';
-import PetShelterDetails from './PetShelterDetails'
 
 const PetDetails = () => {
   const [pets, setData] = useState([]);
+  const [shelter, setShelter] = useState([]);
 
   useEffect(() => {
-    const url = "http://localhost:8000/api/pet/";
 
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch("http://localhost:8000/api/pet/");
         const json = await response.json();
         setData(json);
       } catch (error) {
@@ -18,13 +18,25 @@ const PetDetails = () => {
       }
     };
 
+    const fetchShelterDetails = async () => {
+      await axios
+        .get('http://localhost:8000/api/shelter/find/')
+        .then((res) => {
+          console.log(res.data)
+          setShelter(res.data)
+        })
+        .catch((err) => console.log(err));
+    }
+
+    fetchShelterDetails()
     fetchData();
+
   }, []);
 
   return (
     <div className='all-pets'>
       <div className='row'>
-        {pets.map((pet, index) => {
+        {pets.map((pet) => {
           if (String(pet.id) == String(window.location.href.match(/\/([^\/]+)\/?$/)[1])) {
           return (
           <div className='pet col-sm-4' key={pet.id}>
@@ -33,9 +45,19 @@ const PetDetails = () => {
                 ? <img src='http://localhost:8000/images/paw.png' className="img-fluid"></img>
                 : <img src={pet.image} className="img-fluid img-sizer"></img>
               } 
-               <p> {PetShelterDetails(pet.shelter)} </p>
             </div>
             <p className='name'>{pet.name}</p>
+            {shelter.map((shelter) => {
+              if (shelter.id == pet.shelter) {
+                return (
+                  <div className='shelter col-sm-20' key={shelter.id}>
+                    <p className='name'>Name: {shelter.name}</p>
+                    <p className='email'>Email: { shelter.email}</p>
+                    <p className='phone_number'>Phone Number: {shelter.phone_number}</p>
+                  </div>
+                )}
+              }
+          )}
            
           </div>
         )}}
